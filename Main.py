174b4c95c35707fea99ae10848e4fb4596111e8f
Bernie <95599456+Unforgettableeternalproject@users.noreply.github.com>
2022-04-08 11:50:51 +0800

    Added user interface in console 4/8

--- Main.py
+++ Main.py
@@ -8,8 +8,27 @@ files = []
 init = int(time.strftime('%H', time.localtime()))-1
 runtime = 0
 index = 0
+print("Program initiating...\nEnter arguments(Leave blank for default):\n\nFilename: ", end="")
+filename = input()
+if(filename == ""): 
+    filename = "第十組觀測資料"
+    print(filename, end="")
 
-while runtime != init:
+print("\nAssumed run cycle count: ", end="")
+cycle = int(input())
+if(cycle == ""):
+    cycle = 10
+    print(cycle, end="")
+
+print("\nAssumed run gap(seconds): ", end="")
+gap = int(input())
+if(gap == ""):
+    gap = 10
+    print(gap, end = "")
+
+print("\n\nArgument accepted, running program...\n")
+
+while cycle != 0:
     
     #迴圈每小時執行一次
     file = []
@@ -34,20 +53,31 @@ while runtime != init:
     
     #告知主控端
     runtime = int(time.strftime('%H', time.localtime()))
+    cycle -= 1
     index += 1
-    print("Current Time: " + time.strftime('%y/%m/%d %H:%M', time.localtime()) + f"\nFetching Operation Had Done {index} Time(s).\n\n")
+    print("Current Time: " + time.strftime('%Y/%m/%d %H:%M', time.localtime()) + f"\nFetching Operation Had Done {index} Time(s).\n\n")
     
     #等待
-    time.sleep(3600)
+    time.sleep(gap)
 
 wb = openpyxl.Workbook()
-sheet = wb.create_sheet("第十組觀測資料", 0)
+sheet = wb.create_sheet(filename, 0)
     
 titles = ("紀錄時間", "淨發電量", "發電量比", "風速", "風向")
 sheet.append(titles)
  
 for file in files:
 	sheet.append(file)
- 
+sheet.append("windyAPI designed by Bernie.")
 #輸出
-wb.save("第十組觀測資料.xlsx")
+print("Attempting to save the data as .xlsx file...")
+
+try:
+ wb.save(filename + ".xlsx")
+ print("Saving process compeleted.")
+except:
+ wb.save(f"{time.strftime('%m/%d %H:%M', time.localtime())}.xlsx")
+ print(f"Error occured when saving, backup saved into {time.strftime('%m/%d %H:%M', time.localtime())}.xlsx instead.")
+
+ print("\n\nProgram closing...")
+
